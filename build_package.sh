001fbfe3ea09db47151ae74b812131a56cbe8093
Aibulatov Alex <zap.aibulatov@gmail.com>
2014-07-17 01:58:49 +0600

    Fix build_package.sh

--- build_package.sh
+++ build_package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python2.7 setup.py bdist_egg upload --identity="Aleksandr Aibulatov" --sign --quiet
 python2.7 setup.py bdist_wininst --target-version=2.7 register upload --identity="Aleksandr Aibulatov" --sign --quiet
-setup.py sdist upload --identity="Aleksandr Aibulatov" --sign
+python setup.py sdist upload --identity="Aleksandr Aibulatov" --sign
